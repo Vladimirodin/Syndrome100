26e0e75baf4031d62c2a1d0d5a9007349eb9a0fa
Vladimir Odintcev <vovaodintcev@gmail.com>
2022-09-20 17:15:35 +0500

    add README

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -19,11 +19,23 @@ public class StatisticsServiceTest {
     }
 
     @Test
-    void showMax() {
+    void showMaxBeginning() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {1, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long[] incomesInBillions = {0, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxEnd() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12, 100};
+        long expected = 100;
 
         long actual = service.findMax(incomesInBillions);
 
